9ee7f0512eb3f41405b983a2991a06d2c1d2ae1a
Rahul <rahulmeenap401@gmail.com>
2024-08-27 17:30:51 +0530

    manage upload image in profile page

--- app.js
+++ app.js
@@ -1,22 +1,25 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-var cors = require("cors");
+const cors = require("cors");
+const path = require("path"); 
 
 const connectDB = require("./config/db");
 const userRoutes = require("./routes/v1/userRoutes");
 require("dotenv").config();
 
 const app = express();
-const PORT = process.env.PORT || 3000; // eslint-disable-line no-undef
-app.use(cors());
-
-connectDB();
+const PORT = process.env.PORT || 3000;
 
 // Middleware
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Routes
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+
+connectDB();
+
+
 app.use("/api/v1/user", userRoutes);
 
 app.listen(PORT, () => {
