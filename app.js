4bb3f19fb770606074f2a667658bd1a1b1a00cc0
root <root@ip-172-31-30-124.ap-south-1.compute.internal>
2025-02-03 07:05:44 +0000

    set ssl certificate

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const https = require('https');
+const fs = require('fs');
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
@@ -7,6 +9,14 @@ const connectDB = require("./config/db");
 const userRoutes = require("./routes/v1/userRoutes");
 const demoRoutes = require("./routes/v1/demoRoutes");
 require("dotenv").config();
+
+const privateKey = fs.readFileSync('/var/www/ssl/playground.mprompto.com/playground.mprompto.com.key');
+const certificate = fs.readFileSync('/var/www/ssl/playground.mprompto.com/ssl-bundle.crt');
+
+const credentials = {
+  key: privateKey,
+  cert: certificate,
+};
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -23,6 +33,8 @@ connectDB();
 app.use("/api/v1/user", userRoutes);
 app.use("/api/v1/demo", demoRoutes);
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+//app.listen(PORT, () => {
+//  console.log(`Server is running on port ${PORT}`);
+//});
+const httpsServer = https.createServer(credentials, app);
+httpsServer.listen(PORT);
