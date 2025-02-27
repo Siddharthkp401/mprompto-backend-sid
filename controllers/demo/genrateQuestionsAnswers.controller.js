0bd83ae95326d82156835be6e731d5b820f3ade2
Rahul284-hash <rahulmeenap401@gmail.com>
2025-02-27 11:01:30 +0530

    Add status in response

--- controllers/demo/genrateQuestionsAnswers.controller.js
+++ controllers/demo/genrateQuestionsAnswers.controller.js
@@ -6,12 +6,12 @@ exports.generateQAndA = async (req, res) => {
         const { id, question_generation_prompt, answer_generation_prompt, primary_text, secondary_text } = req.body;
 
         if (!id || !question_generation_prompt || !answer_generation_prompt) {
-            return res.status(400).json({ message: "Missing required fields" });
+            return res.status(400).json({ message: "Missing required fields", status: false });
         }
 
         const client = await DemoClient.findById(id);
         if (!client) {
-            return res.status(404).json({ message: "Client not found" });
+            return res.status(404).json({ message: "Client not found", status: false });
         }
 
         client.primary_text = primary_text || client.primary_text;
@@ -28,14 +28,14 @@ exports.generateQAndA = async (req, res) => {
             answer_prompt: answer_generation_prompt,
         });
 
-
         // Respond with AI API response
         return res.status(200).json({
             message: "Q&A generated successfully",
             aiResponse: aiApiResponse.data,
+            status: true
         });
     } catch (error) {
         console.error("Error generating Q&A:", error);
-        return res.status(500).json({ message: "Internal Server Error" });
+        return res.status(500).json({ message: "Internal Server Error", status: false });
     }
 };
--- controllers/demo/updateQAStatus.controller.js
+++ controllers/demo/updateQAStatus.controller.js
@@ -4,9 +4,9 @@ exports.updateQAStatus = async (req, res) => {
     try {
         const { id, q_and_a_status } = req.body;
 
-        const validStatuses = ["-", "In-Progress","Completed","Error"];
+        const validStatuses = ["-", "In-Progress", "Completed", "Error"];
         if (!validStatuses.includes(q_and_a_status)) {
-            return res.status(400).json({ message: "Invalid status value" });
+            return res.status(400).json({ message: "Invalid status value", status: false });
         }
 
         const updatedClient = await DemoClient.findByIdAndUpdate(
@@ -16,15 +16,15 @@ exports.updateQAStatus = async (req, res) => {
         );
 
         if (!updatedClient) {
-            return res.status(404).json({ message: "Client not found" });
+            return res.status(404).json({ message: "Client not found", status: false });
         }
 
         res.status(200).json({
             message: "Q&A Status updated successfully",
             data: updatedClient,
+            status: true
         });
     } catch (error) {
-        res.status(500).json({ message: "Internal server error", error: error.message });
+        res.status(500).json({ message: "Internal server error", error: error.message, status: false });
     }
 };
-
